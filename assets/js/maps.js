efd9e2187fa44ab4534bac509db0bf2ba7bbf88f
Nicolette Shaw <nikkikobako@gmail.com>
2020-11-20 08:28:54 +0000

    Add:Google location and a marker for a fictional location

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,28 +1,20 @@
 function initMap() {
   var map = new google.maps.Map(document.getElementById("rescue-map"), {
-    zoom: 3,
+    zoom: 10,
     center: {
-      lat: 46.619261,
-      lng: -33.134766,
+      lat: 53.4387125,
+      lng: -2.2690649,
     },
   });
 
 
 
- /* var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+ var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
   var locations = [
     {
-      lat: 40.785091,
-      lng: -73.968285,
-    },
-    {
-      lat: 41.084045,
-      lng: -73.874256,
-    },
-    {
-      lat: 40.754932,
-      lng: -73.984016,
+      lat: 53.4387125,
+      lng: -2.2690649,
     },
   ];
 
@@ -37,5 +29,4 @@ function initMap() {
     imagePath:
       "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
   });
-  */
 }
